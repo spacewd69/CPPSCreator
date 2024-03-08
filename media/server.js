32fae2153dfb114444d7e5231c6827d68c3f377a
SpaceWD <121475790+spacewd69@users.noreply.github.com>
2024-03-08 11:20:10 -0500

    Update server.js

--- media/server.js
+++ media/server.js
@@ -1,1 +1,5 @@
-
+//server
+require("/media/servers.xml") //this gets server ports and ids
+start("port=6112") start("port=9875") //starts login & redemption servers
+start("id=3100") start("id=3101") start("id=3102") start("id=3103") //starts game servers
+//this is a proof of concept for a cpps server engine and may not work. I am still very inexperienced with JS
